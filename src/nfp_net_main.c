324611439913f5718af898862d263348937eee9d
Jakub Kicinski <jakub.kicinski@netronome.com>
2015-10-30 13:59:11 +0000

    nfp_net_main: use dma_set_mask_and_coherent()
    We already use dma_set_mask_and_coherent() to set both masks
    in the VF driver.  While at it use the appropriate define
    and drop the error message for this super-unlikely error.
    Also move the call after pci_request_regions() so that the
    ordering is closer to the VF driver.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Acked-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>
    Reviewed-by: Jason S. McMullan <jason.mcmullan@netronome.com>

--- src/nfp_net_main.c
+++ src/nfp_net_main.c
@@ -623,23 +623,16 @@ static int nfp_net_pci_probe(struct pci_dev *pdev,
 
 	pci_set_master(pdev);
 
-	err = pci_set_dma_mask(pdev,  DMA_BIT_MASK(40));
-	if (err < 0) {
-		dev_err(&pdev->dev, "Cannot set DMA mask\n");
-		goto err_pci_disable;
-	}
-
-	err = pci_set_consistent_dma_mask(pdev,  DMA_BIT_MASK(40));
-	if (err < 0) {
-		dev_err(&pdev->dev, "Cannot set consistent DMA mask\n");
-		goto err_pci_disable;
-	}
-
 	err = pci_request_regions(pdev, nfp_net_driver_name);
 	if (err < 0) {
 		dev_err(&pdev->dev, "Unable to reserve pci resources.\n");
 		goto err_pci_disable;
 	}
+
+	err = dma_set_mask_and_coherent(&pdev->dev,
+					DMA_BIT_MASK(NFP_NET_MAX_DMA_BITS));
+	if (err)
+		goto err_pci_regions;
 
 	switch (pdev->device) {
 	case PCI_DEVICE_NFP3200:
