a7f98ce07e34e8696e7e8e6e52e05bb59f39e026
Jakub Kicinski <jakub.kicinski@netronome.com>
2015-12-30 19:26:23 +0000

    nfp_net_common: correct RX buffer length calculation
    When calculating the RX buffer length we need to account for
    up to 2 VLAN tags.  Rounding up to 1k is an relic of a distant
    past and can be removed.  While at it also remove trivial print
    statement.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Acked-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    Reviewed-by: Dinan Gunawardena <dinan.gunawardena@netronome.com>

--- src/nfp_net_common.c
+++ src/nfp_net_common.c
@@ -1934,9 +1934,6 @@ static void nfp_net_set_rx_mode(struct net_device *netdev)
 static int nfp_net_change_mtu(struct net_device *netdev, int new_mtu)
 {
 	struct nfp_net *nn = netdev_priv(netdev);
-	u32 tmp;
-
-	nn_dbg(nn, "New MTU = %d\n", new_mtu);
 
 	if (new_mtu < 68 || new_mtu > nn->max_mtu) {
 		nn_err(nn, "New MTU (%d) is not valid\n", new_mtu);
@@ -1944,10 +1941,7 @@ static int nfp_net_change_mtu(struct net_device *netdev, int new_mtu)
 	}
 
 	netdev->mtu = new_mtu;
-
-	/* Freelist buffer size rounded up to the nearest 1K */
-	tmp = new_mtu + ETH_HLEN + VLAN_HLEN + NFP_NET_MAX_PREPEND;
-	nn->fl_bufsz = roundup(tmp, 1024);
+	nn->fl_bufsz = NFP_NET_MAX_PREPEND + ETH_HLEN + VLAN_HLEN * 2 + new_mtu;
 
 	/* restart if running */
 	if (netif_running(netdev)) {
