dea1f87c905c4b72bc1653576075a274248b4527
Jakub Kicinski <jakub.kicinski@netronome.com>
2016-12-13 16:47:05 +0000

    nfpcore: simplify CRC helpers
    CRC helpers are quite trivial, they can be further simplified
    and turned into static inline functions.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Dinan Gunawardena <dinan.gunawardena@netronome.com>

--- src/nfpcore/crc32.h
+++ src/nfpcore/crc32.h
@@ -31,14 +31,35 @@
  * SOFTWARE.
  */
 
-#ifndef CRC32_H
-#define CRC32_H
+#ifndef NFP_CRC32_H
+#define NFP_CRC32_H
 
-u32 crc32_posix_add(u32 crc, const void *buff, size_t len);
-u32 crc32_posix_end(u32 crc, size_t total_len);
-static inline u32 crc32_posix(const void *buff, size_t len)
+#include <linux/kernel.h>
+#include <linux/crc32.h>
+
+/**
+ * crc32_posix_end() - Finalize POSIX CRC32 working state
+ * @crc:	Current CRC32 working state
+ * @total_len:	Total length of data that was CRC32'd
+ *
+ * Return: Final POSIX CRC32 value
+ */
+static inline u32 crc32_posix_end(u32 crc, size_t total_len)
 {
-	return crc32_posix_end(crc32_posix_add(0, buff, len), len);
+	/* Extend with the length of the string. */
+	while (total_len != 0) {
+		u8 c = total_len & 0xff;
+
+		crc = crc32_be(crc, &c, 1);
+		total_len >>= 8;
+	}
+
+	return ~crc;
 }
 
-#endif /* CRC32_H */
+static inline u32 crc32_posix(const void *buff, size_t len)
+{
+	return crc32_posix_end(crc32_be(0, buff, len), len);
+}
+
+#endif /* NFP_CRC32_H */
