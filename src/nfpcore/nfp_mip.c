38cee9a4896ae9a097d7d3a8deb4588f9bfa9d38
Jakub Kicinski <jakub.kicinski@netronome.com>
2016-12-15 18:31:16 +0000

    nfpcore: fix endian issues with nfp_mip code
    Most of the nfp_mip code can be removed since we never make use
    of the MIP entries.  Keep the MIP header structure in little
    endian and byteswap when needed.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Dinan Gunawardena <dinan.gunawardena@netronome.com>

--- src/nfpcore/nfp_mip.c
+++ src/nfpcore/nfp_mip.c
@@ -36,7 +36,6 @@
  * Authors: Jason McMullan <jason.mcmullan@netronome.com>
  *          Espen Skoglund <espen.skoglund@netronome.com>
  */
-
 #include <linux/kernel.h>
 
 #include "nfp.h"
@@ -44,151 +43,29 @@
 #include "nfp_nffw.h"
 #include "nfp_target.h"
 
-#define NFP_MIP_SIGNATURE	0x0050494d  /* "MIP\0" */
+#define NFP_MIP_SIGNATURE	cpu_to_le32(0x0050494d)  /* "MIP\0" */
+#define NFP_MIP_VERSION		cpu_to_le32(1)
 #define NFP_MIP_MAX_OFFSET	(256 * 1024)
 
-#define UINT32_MAX	(0xffffffff)
-
-#define NFP_MIP_VERSION         1
-#define NFP_MIP_QC_VERSION      1
-#define NFP_MIP_VPCI_VERSION    1
-
-enum nfp_mip_entry_type {
-	NFP_MIP_TYPE_NONE = 0,
-	NFP_MIP_TYPE_QC = 1,
-	NFP_MIP_TYPE_VPCI = 2,
-};
-
 struct nfp_mip {
-	u32 signature;
-	u32 mip_version;
-	u32 mip_size;
-	u32 first_entry;
+	__le32 signature;
+	__le32 mip_version;
+	__le32 mip_size;
+	__le32 first_entry;
 
-	u32 version;
-	u32 buildnum;
-	u32 buildtime;
-	u32 loadtime;
+	__le32 version;
+	__le32 buildnum;
+	__le32 buildtime;
+	__le32 loadtime;
 
-	u32 symtab_addr;
-	u32 symtab_size;
-	u32 strtab_addr;
-	u32 strtab_size;
+	__le32 symtab_addr;
+	__le32 symtab_size;
+	__le32 strtab_addr;
+	__le32 strtab_size;
 
 	char name[16];
 	char toolchain[32];
 };
-
-struct nfp_mip_entry {
-	u32 type;
-	u32 version;
-	u32 offset_next;
-};
-
-struct nfp_mip_qc {
-	u32 type;
-	u32 version;
-	u32 offset_next;
-	u32 type_config;
-	u32 type_config_size;
-	u32 host_config;
-	u32 host_config_size;
-	u32 config_signal;
-	u32 nfp_queue_size;
-	u32 queue_base;
-	u32 sequence_base;
-	u32 sequence_type;
-	u32 status_base;
-	u32 status_version;
-	u32 error_base;
-};
-
-struct nfp_mip_vpci {
-	u32 type;
-	u32 version;
-	u32 offset_next;
-	u32 vpci_epconfig;
-	u32 vpci_epconfig_size;
-};
-
-static void __mip_update_byteorder(struct nfp_mip *mip)
-{
-	struct nfp_mip_entry *ent;
-	int offset;
-
-	/* Convert main MIP structure. */
-	mip->signature = le32_to_cpu(mip->signature);
-	mip->mip_version = le32_to_cpu(mip->mip_version);
-	mip->mip_size = le32_to_cpu(mip->mip_size);
-	mip->first_entry = le32_to_cpu(mip->first_entry);
-	mip->version = le32_to_cpu(mip->version);
-	mip->buildnum = le32_to_cpu(mip->buildnum);
-	mip->buildtime = le32_to_cpu(mip->buildtime);
-	mip->loadtime = le32_to_cpu(mip->loadtime);
-	mip->symtab_addr = le32_to_cpu(mip->symtab_addr);
-	mip->symtab_size = le32_to_cpu(mip->symtab_size);
-	mip->strtab_addr = le32_to_cpu(mip->strtab_addr);
-	mip->strtab_size = le32_to_cpu(mip->strtab_size);
-
-	/* Convert known MIP entries. */
-	for (offset = mip->first_entry;
-		 (offset + sizeof(*ent)) < mip->mip_size;
-		 offset += ent->offset_next) {
-		ent = (struct nfp_mip_entry *)(((char *)mip) + offset);
-		ent->type = le32_to_cpu(ent->type);
-		ent->version = le32_to_cpu(ent->version);
-		ent->offset_next = le32_to_cpu(ent->offset_next);
-
-		if ((offset + ent->offset_next) > mip->mip_size)
-			break;
-
-		switch (ent->type) {
-		case NFP_MIP_TYPE_NONE:
-			return;
-
-		case NFP_MIP_TYPE_QC:
-		{
-			struct nfp_mip_qc *qc = (struct nfp_mip_qc *)ent;
-
-			if (qc->version != NFP_MIP_QC_VERSION)
-				break;
-			qc->type_config = le32_to_cpu(qc->type_config);
-			qc->type_config_size =
-				le32_to_cpu(qc->type_config_size);
-			qc->host_config = le32_to_cpu(qc->host_config);
-			qc->host_config_size =
-				le32_to_cpu(qc->host_config_size);
-			qc->config_signal = le32_to_cpu(qc->config_signal);
-			qc->nfp_queue_size = le32_to_cpu(qc->nfp_queue_size);
-			qc->queue_base = le32_to_cpu(qc->queue_base);
-			qc->sequence_base = le32_to_cpu(qc->sequence_base);
-			qc->sequence_type = le32_to_cpu(qc->sequence_type);
-			qc->status_base = le32_to_cpu(qc->status_base);
-			qc->status_version = le32_to_cpu(qc->status_version);
-			qc->error_base = le32_to_cpu(qc->error_base);
-			break;
-		}
-
-		case NFP_MIP_TYPE_VPCI:
-		{
-			struct nfp_mip_vpci *vpci = (struct nfp_mip_vpci *)ent;
-
-			if (vpci->version != NFP_MIP_VPCI_VERSION)
-				break;
-			vpci->vpci_epconfig =
-				le32_to_cpu(vpci->vpci_epconfig);
-			vpci->vpci_epconfig_size =
-				le32_to_cpu(vpci->vpci_epconfig_size);
-			break;
-		}
-
-		default:
-			ent->type = cpu_to_le32(ent->type);
-			ent->version = cpu_to_le32(ent->version);
-			break;
-		}
-	}
-}
 
 /**
  * nfp_mip() - Get MIP for NFP device.
@@ -254,7 +131,7 @@ static int nfp_mip_nfp6000_mu_locality_lsb(struct nfp_cpp *cpp)
 
 static int __nfp_mip_location(struct nfp_cpp *cpp,
 			      u32 *cppid, u64 *addr,
-			      unsigned long *size, unsigned long *load_time)
+			      unsigned long *size, __le32 *load_time)
 {
 	int retval;
 	u32 mip_cppid = 0;
@@ -293,7 +170,7 @@ static int __nfp_mip_location(struct nfp_cpp *cpp,
 				      mip_off,
 				      &mip, sizeof(mip));
 		if (retval < sizeof(mip) ||
-		    le32_to_cpu(mip.signature) != NFP_MIP_SIGNATURE)
+		    mip.signature != NFP_MIP_SIGNATURE)
 			mip_cppid = 0;
 	}
 
@@ -309,7 +186,7 @@ static int __nfp_mip_location(struct nfp_cpp *cpp,
 					      &mip, sizeof(mip));
 			if (retval < sizeof(mip))
 				goto err_probe;
-			if (le32_to_cpu(mip.signature) == NFP_MIP_SIGNATURE) {
+			if (mip.signature == NFP_MIP_SIGNATURE) {
 				mip_cppid = cpp_id;
 				break;
 			}
@@ -327,7 +204,7 @@ static int __nfp_mip_location(struct nfp_cpp *cpp,
 	*cppid = mip_cppid;
 	*addr = mip_off;
 	*size = (le32_to_cpu(mip.mip_size) + 7) & ~7;
-	*load_time = le32_to_cpu(mip.loadtime);
+	*load_time = mip.loadtime;
 	return 0;
 
 err_probe:
@@ -348,7 +225,8 @@ err_probe:
  */
 int nfp_mip_probe(struct nfp_cpp *cpp)
 {
-	unsigned long size, time;
+	unsigned long size;
+	__le32 time;
 	u32 cpp_id;
 	u64 addr;
 	struct nfp_mip *mip;
@@ -389,13 +267,11 @@ int nfp_mip_probe(struct nfp_cpp *cpp)
 		return (retval < 0) ? retval : -EIO;
 	}
 
-	if ((le32_to_cpu(mip->signature) != NFP_MIP_SIGNATURE) ||
-	    (le32_to_cpu(mip->mip_version) != NFP_MIP_VERSION)) {
+	if (mip->signature != NFP_MIP_SIGNATURE ||
+	    mip->mip_version != NFP_MIP_VERSION) {
 		kfree(mip);
 		return -EIO;
 	}
-
-	__mip_update_byteorder(mip);
 
 	nfp_mip_cache_set(cpp, mip);
 
@@ -416,9 +292,9 @@ int nfp_mip_symtab(const struct nfp_mip *mip, u32 *addr, u32 *size)
 		return -EINVAL;
 
 	if (addr)
-		*addr = mip->symtab_addr;
+		*addr = le32_to_cpu(mip->symtab_addr);
 	if (size)
-		*size = mip->symtab_size;
+		*size = le32_to_cpu(mip->symtab_size);
 
 	return 0;
 }
@@ -437,9 +313,9 @@ int nfp_mip_strtab(const struct nfp_mip *mip, u32 *addr, u32 *size)
 		return -EINVAL;
 
 	if (addr)
-		*addr = mip->strtab_addr;
+		*addr = le32_to_cpu(mip->strtab_addr);
 	if (size)
-		*size = mip->strtab_size;
+		*size = le32_to_cpu(mip->strtab_size);
 
 	return 0;
 }
