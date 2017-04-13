8bdbeccbb403b61050fb3efd6ee00dd7cd9e0cb5
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-04-12 17:02:21 -0700

    nfp: move bpf offload code to the BPF app
    Move bulk of the eBPF offload code out of common vNIC code into
    app-specific callbacks.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Nic Viljoen <nick.viljoen@netronome.com>

--- src/nfp_net.h
+++ src/nfp_net.h
@@ -876,18 +876,9 @@ static inline void nfp_net_debugfs_dir_clean(struct dentry **dir)
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
 void nfp_net_filter_stats_timer(unsigned long data);
-int nfp_net_bpf_offload(struct nfp_net *nn, struct tc_cls_bpf_offload *cls_bpf);
 #else
-struct tc_cls_bpf_offload;
-
 static inline void nfp_net_filter_stats_timer(unsigned long data)
 {
-}
-
-static inline int
-nfp_net_bpf_offload(struct nfp_net *nn, struct tc_cls_bpf_offload *cls_bpf)
-{
-	return -EOPNOTSUPP;
 }
 #endif
 
