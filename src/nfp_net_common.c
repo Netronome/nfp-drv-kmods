ee6e1949054edf3921e768bd379f9cb4d8a24357
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-01-06 22:34:45 -0800

    nfp: always unmask aux interrupts at init
    The link state and exception interrupts may be masked when we probe.
    The firmware should in theory prevent sending (and automasking) those
    interrupts if the device is disabled, but if my reading of the FW code
    is correct there are firmwares out there with race conditions in this
    area.  The interrupt may also be masked if previous driver which used
    the device was malfunctioning and we didn't load the FW (there is no
    other good way to comprehensively reset the PF).
    
    Note that FW unmasks the data interrupts by itself when vNIC is
    enabled, such helpful operation is not performed for LSC/EXN interrupts.
    
    Always unmask the auxiliary interrupts after request_irq().  On the
    remove path add missing PCI write flush before free_irq().
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

--- src/nfp_net_common.c
+++ src/nfp_net_common.c
@@ -593,6 +593,7 @@ nfp_net_aux_irq_request(struct nfp_net *nn, u32 ctrl_offset,
 		return err;
 	}
 	nn_writeb(nn, ctrl_offset, entry->entry);
+	nfp_net_irq_unmask(nn, entry->entry);
 
 	return 0;
 }
@@ -607,6 +608,7 @@ static void nfp_net_aux_irq_free(struct nfp_net *nn, u32 ctrl_offset,
 				 unsigned int vector_idx)
 {
 	nn_writeb(nn, ctrl_offset, 0xff);
+	nn_pci_flush(nn);
 	free_irq(nn->irq_entries[vector_idx].vector, nn);
 }
 
