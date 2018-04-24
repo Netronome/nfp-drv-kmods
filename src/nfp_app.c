3f02a7ad29949b977668e687c86af87e9352e0f1
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-04-23 19:59:07 -0700

    compat: align defines after Linux 4.17 rc1 release
    Linux 4.17-rc1 has been released.  We can now depend on the version.
    Remember to bump the module version, we are working on 4.18 now.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

--- src/nfp_app.c
+++ src/nfp_app.c
@@ -48,7 +48,7 @@
 static const struct nfp_app_type *apps[] = {
 #ifdef CONFIG_NFP_NET_PF
 	[NFP_APP_CORE_NIC]	= &app_nic,
-#if LINUX_RELEASE_4_17 && \
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0) && \
     defined(CONFIG_BPF_SYSCALL)
 	[NFP_APP_BPF_NIC]	= &app_bpf,
 #else
