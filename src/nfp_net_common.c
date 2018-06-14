c9fa2ff4d58af9f41b82065d2aa0881fd12a0fdd
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-06-14 14:22:33 -0700

    xdp: don't make drivers report attachment mode
    prog_attached of struct netdev_bpf should have been superseded
    by simply setting prog_id long time ago, but we kept it around
    to allow offloading drivers to communicate attachment mode (drv
    vs hw).  Subsequently drivers were also allowed to report back
    attachment flags (prog_flags), and since nowadays only programs
    attached will XDP_FLAGS_HW_MODE can get offloaded, we can tell
    the attachment mode from the flags driver reports.  Remove
    prog_attached member.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>

--- src/nfp_net_common.c
+++ src/nfp_net_common.c
@@ -3563,9 +3563,11 @@ static int nfp_net_xdp(struct net_device *netdev, struct netdev_bpf *xdp)
 		return nfp_net_xdp_setup(nn, xdp->prog, compat__xdp_flags(xdp),
 					 compat__xdp_extact(xdp));
 	case XDP_QUERY_PROG:
+#if !LINUX_RELEASE_4_19
 		xdp->prog_attached = !!nn->xdp_prog;
 		if (nn->dp.bpf_offload_xdp)
 			xdp->prog_attached = XDP_ATTACHED_HW;
+#endif
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
 		xdp->prog_id = nn->xdp_prog ? nn->xdp_prog->aux->id : 0;
 #endif
