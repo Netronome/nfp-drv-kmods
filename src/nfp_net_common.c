7f80b83e079388ea10be2df0ef13307ea1c9c28d
Jakub Kicinski <jakub.kicinski@netronome.com>
2019-03-25 17:51:04 -0700

    nfp: move vNIC reset before netdev init
    During probe we clear vNIC configuration in case the device
    wasn't closed cleanly by previous driver.  Move that code
    before netdev init, so netdev init can already try to apply
    its config parameters.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- src/nfp_net_common.c
+++ src/nfp_net_common.c
@@ -4181,9 +4181,6 @@ int nfp_net_init(struct nfp_net *nn)
 		nn->dp.ctrl |= NFP_NET_CFG_CTRL_IRQMOD;
 	}
 
-	if (nn->dp.netdev)
-		nfp_net_netdev_init(nn);
-
 	/* Stash the re-configuration queue away.  First odd queue in TX Bar */
 	nn->qcp_cfg = nn->tx_bar + NFP_QCP_QUEUE_ADDR_SZ;
 
@@ -4195,6 +4192,9 @@ int nfp_net_init(struct nfp_net *nn)
 				   NFP_NET_CFG_UPDATE_GEN);
 	if (err)
 		return err;
+
+	if (nn->dp.netdev)
+		nfp_net_netdev_init(nn);
 
 	nfp_net_vecs_init(nn);
 
