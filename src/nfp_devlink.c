4fc5453085d88b8847bf36cae47f9f618b775f55
Dirk van der Merwe <dirk.vandermerwe@netronome.com>
2019-04-08 19:35:58 -0700

    nfp: pass switch ID through devlink_port_attrs_set()
    Pass the switch ID down the to devlink through devlink_port_attrs_set()
    so it can be used by devlink_compat_switch_id_get().
    
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- src/nfp_devlink.c
+++ src/nfp_devlink.c
@@ -381,7 +381,9 @@ const struct devlink_ops nfp_devlink_ops = {
 int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 {
 	struct nfp_eth_table_port eth_port;
+	int __maybe_unused serial_len;
 	struct devlink *devlink;
+	const u8 *serial;
 	int ret;
 
 	rtnl_lock();
@@ -390,12 +392,13 @@ int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 	if (ret)
 		return ret;
 
+	serial_len = nfp_cpp_serial(port->app->cpp, &serial);
 	devlink_port_attrs_set(&port->dl_port, DEVLINK_PORT_FLAVOUR_PHYSICAL,
 			       eth_port.label_port, eth_port.is_split,
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 2, 0)
 			       eth_port.label_subport);
 #else
-			       eth_port.label_subport, NULL, 0);
+			       eth_port.label_subport, serial, serial_len);
 #endif
 
 	devlink = priv_to_devlink(app->pf);
