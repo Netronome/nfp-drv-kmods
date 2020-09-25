55c71c7b920e1386ea0567031c3e19b0cfc4ecb6
Jacob Keller <jacob.e.keller@intel.com>
2020-09-25 13:46:05 -0700

    devlink: check flash_update parameter support in net core
    When implementing .flash_update, drivers which do not support
    per-component update are manually checking the component parameter to
    verify that it is NULL. Without this check, the driver might accept an
    update request with a component specified even though it will not honor
    such a request.
    
    Instead of having each driver check this, move the logic into
    net/core/devlink.c, and use a new `supported_flash_update_params` field
    in the devlink_ops. Drivers which will support per-component update must
    now specify this by setting DEVLINK_SUPPORT_FLASH_UPDATE_COMPONENT in
    the supported_flash_update_params in their devlink_ops.
    
    This helps ensure that drivers do not forget to check for a NULL
    component if they do not support per-component update. This also enables
    a slightly better error message by enabling the core stack to set the
    netlink bad attribute message to indicate precisely the unsupported
    attribute in the message.
    
    Going forward, any new additional parameter to flash update will require
    a bit in the supported_flash_update_params bitfield.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Jakub Kicinski <kuba@kernel.org>
    Cc: Jiri Pirko <jiri@mellanox.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Michael Chan <michael.chan@broadcom.com>
    Cc: Bin Luo <luobin9@huawei.com>
    Cc: Saeed Mahameed <saeedm@mellanox.com>
    Cc: Leon Romanovsky <leon@kernel.org>
    Cc: Ido Schimmel <idosch@mellanox.com>
    Cc: Danielle Ratson <danieller@mellanox.com>
    Cc: Shannon Nelson <snelson@pensando.io>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>
    Signed-off-by: Louis Peens <louis.peens@netronome.com>

--- src/nfp_devlink.c
+++ src/nfp_devlink.c
@@ -364,8 +364,10 @@ static int
 nfp_devlink_flash_update(struct devlink *devlink, const char *path,
 			 const char *component, struct netlink_ext_ack *extack)
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 10, 0)
 	if (component)
 		return -EOPNOTSUPP;
+#endif
 	return nfp_flash_update_common(devlink_priv(devlink), path, extack);
 }
 #endif
