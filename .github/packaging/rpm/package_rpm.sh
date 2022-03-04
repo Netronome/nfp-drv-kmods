14b0024070285ff43814135885848fdb2faf4528
Walter Heymans <walter.heymans@corigine.com>
2022-03-04 12:16:13 +0200

    packaging: dkms: rpm: add DKMS rpm build script
    This patch adds support to build DKMS RPM packages of the
    nfp driver. An RPM spec.in file is added which is used
    when building the package. From DKMS version 3, support
    was removed to build RPM packages directly from DKMS.
    This is why this patch introduces a manual build process
    instead of using DKMS packaging tools.
    
    - The spec.in file is a modified version of the one
      included in previous versions of DKMS. The file was
      updated to include the correct package name, version,
      company information and other modifications required to
      get a working build.
    
    Signed-off-by: Walter Heymans <walter.heymans@corigine.com>
    Reviewed-by: Charles Murcott <charles.murcott@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>

---
+++ .github/packaging/rpm/package_rpm.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+usage() {
+    cat <<EOH
+
+Usage: .github/packaging/rpm/package_rpm.sh $NFP_DRV_REPO $PKG_NAME $PACKAGE_VERSION $PACKAGE_REVISION
+
+Copyright (C) 2022 Corigine, Inc
+
+Builds DKMS RPM packages. This script should not be run manually, instead it should be called
+from the package-dkms.sh script under the scripts directory.
+
+EOH
+    exit 1
+}
+
+if [ "$#" -ne 4 ]; then
+    echo -e "::error::Incorrect arguments\n" 1>&2
+    usage
+    exit 1
+fi
+
+SRCDIR=$1
+echo "SourceDIR: ${SRCDIR}"
+
+# This is to get the actual base dir of the script and
+# not the directory from which the script was called
+BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+BUILDDIR="${SRCDIR}/_build"
+OUTDIR="${SRCDIR}/rpm"
+
+
+PKG_NAME=$2
+PKG_VERSION=$3
+PKG_REVISION=$4
+
+set -xe
+
+cp_from_template () {
+    cp $1 $2
+    sed -i "s/__PKG_VERSION__/${PKG_VERSION}/g" $2
+    sed -i "s/__PKG_NAME__/${PKG_NAME}/g" $2
+}
+
+
+prepare () {
+    echo "Preparing build environment"
+    tmpdir=$(pwd)
+    cd ${SRCDIR}
+    rm -rf ${OUTDIR}/${PKG_NAME}
+    cd ${tmpdir}
+    mkdir -p ${OUTDIR}/${PKG_NAME}
+}
+
+output_manifest () {
+    tmpdir=$(pwd)
+    echo "MANIFEST:${PKG_NAME}-${PKG_VERSION}" > ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    cd ${SRCDIR}
+    tag=$(git rev-parse HEAD)
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    url=$(git config --get remote.origin.url)
+    cd ${tmpdir}
+    echo "DATE:$(date -u +%Y.%m.%d.%H%M)" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "BRANCH:${branch}" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "TAG:${tag}" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "URL:${url}" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "HOST INFO:" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    host_name=$(hostname)
+    kernel_version=$(uname -r)
+    os_info=$(cat /etc/os-release  | head -n 2 | sed 's/NAME/OS_NAME/g' | sed 's/VERSION/OS_VERSION/g')
+    echo "HOSTNAME:${host_name}" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "KERNEL_VERSION:${kernel_version}" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+    echo "${os_info}" >> ${OUTDIR}/${PKG_NAME}/${PKG_NAME}-dkms-${PKG_VERSION}".manifest"
+}
+
+build_nfp_drv_kmod_dkms () {
+    mkdir -p ${BUILDDIR}/${PKG_NAME}
+    cp -r ${SRCDIR}/src ${BUILDDIR}/${PKG_NAME}/src
+    echo ${PKG_VERSION} > ${BUILDDIR}/${PKG_NAME}/.revision
+    cp_from_template ${SRCDIR}/.github/packaging/dkms.conf ${BUILDDIR}/${PKG_NAME}/dkms.conf
+    cp -r ${SRCDIR}/Makefile ${BUILDDIR}/${PKG_NAME}/Makefile
+    ln -sf ${BUILDDIR}/${PKG_NAME} /usr/src/${PKG_NAME}-${PKG_VERSION}
+
+    mkdir -p ${BUILDDIR}/rpmbuild/{BUILD,RPMS,SRPMS,SPECS,SOURCES}
+    cp_from_template ${BASEDIR}/nfp-drv-kmods-dkms.spec.in ${BUILDDIR}/rpmbuild/SPECS/${PKG_NAME}-dkms.spec
+    mkdir -p ${BUILDDIR}/rpmbuild/SOURCES/${PKG_NAME}-${PKG_VERSION}
+    cp ${SRCDIR}/.github/packaging/common.postinst ${BUILDDIR}/rpmbuild/SOURCES
+
+    dkms add ${PKG_NAME}/${PKG_VERSION} --rpm_safe_upgrade
+
+    cp ${BUILDDIR}/rpmbuild/SOURCES/common.postinst \
+    ${BUILDDIR}/rpmbuild/SOURCES/${PKG_NAME}-${PKG_VERSION}/common.postinst
+
+    cp -Lpr /var/lib/dkms/${PKG_NAME}/${PKG_VERSION}/source \
+    ${BUILDDIR}/rpmbuild/SOURCES/${PKG_NAME}-${PKG_VERSION}
+
+    source_working_dir=$(pwd)
+    cd ${BUILDDIR}/rpmbuild
+    rpmbuild --define "_topdir ${BUILDDIR}/rpmbuild" \
+        --define "version ${PKG_VERSION}" \
+        --define "release ${PKG_REVISION}" \
+        --define "module_name ${PKG_NAME}" \
+        --define "mktarball_line --source-only" \
+        --define "__find_provides  /usr/lib/dkms/find-provides" \
+        --define "_use_internal_dependency_generator 0" \
+        -ba ${BUILDDIR}/rpmbuild/SPECS/${PKG_NAME}-dkms.spec
+    cd ${source_working_dir}
+    cp ${BUILDDIR}/rpmbuild/RPMS/noarch/*.rpm ${OUTDIR}/${PKG_NAME}/
+}
+
+cleanup () {
+    echo "CLEANUP"
+    echo "${PKG_NAME}/${PKG_VERSION}"
+    dkms remove ${PKG_NAME}/${PKG_VERSION} --all
+
+    rm -rf /usr/src/${PKG_NAME:?"PKG_NAME not defined"}-${PKG_VERSION:?"PKG_VERSION not defined"}
+    rm -rf ${BUILDDIR:?"BUILDDIR not defined"}
+}
+
+prepare
+build_nfp_drv_kmod_dkms
+output_manifest
+cleanup
