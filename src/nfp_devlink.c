757cfc18f9b94063bdc8eca8f093bb10eac4790d
Gao Xiao <gao.xiao@corigine.com>
2022-09-20 16:43:55 +0800

    nfp: compat: add compat codes for the struct 'devlink_ops'
    The member 'port_split' and 'port_unsplit' of devlink_ops have changed after
    Centos 9 Stream 5.14.0-162. Update the compat to match the above version.
    
    Signed-off-by: Gao Xiao <gao.xiao@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>

--- src/nfp_devlink.c
+++ src/nfp_devlink.c
@@ -29,7 +29,7 @@ nfp_devlink_fill_eth_port(struct nfp_port *port,
 }
 
 static int
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#if VER_NON_RHEL_LT(5, 18) || RHEL_RELEASE_LT(9, 163, 0, 0)
 nfp_devlink_fill_eth_port_from_id(struct nfp_pf *pf, unsigned int port_index,
 #else
 nfp_devlink_fill_eth_port_from_id(struct nfp_pf *pf,
@@ -39,7 +39,7 @@ nfp_devlink_fill_eth_port_from_id(struct nfp_pf *pf,
 {
 	struct nfp_port *port;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#if VER_NON_RHEL_LT(5, 18) || RHEL_RELEASE_LT(9, 163, 0, 0)
 	port = nfp_port_from_id(pf, NFP_PORT_PHYS_PORT, port_index);
 #else
 	port = container_of(dl_port, struct nfp_port, dl_port);
@@ -77,7 +77,7 @@ static int
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0)
 nfp_devlink_port_split(struct devlink *devlink, unsigned int port_index,
 		       unsigned int count)
-#elif LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#elif VER_NON_RHEL_LT(5, 18) || RHEL_RELEASE_LT(9, 163, 0, 0)
 nfp_devlink_port_split(struct devlink *devlink, unsigned int port_index,
 		       unsigned int count, struct netlink_ext_ack *extack)
 #else
@@ -98,7 +98,7 @@ nfp_devlink_port_split(struct devlink *devlink, struct devlink_port *port,
 #endif
 
 	rtnl_lock();
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#if VER_NON_RHEL_LT(5, 18) || RHEL_RELEASE_LT(9, 163, 0, 0)
 	ret = nfp_devlink_fill_eth_port_from_id(pf, port_index, &eth_port);
 #else
 	ret = nfp_devlink_fill_eth_port_from_id(pf, port, &eth_port);
@@ -129,7 +129,7 @@ out:
 static int
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0)
 nfp_devlink_port_unsplit(struct devlink *devlink, unsigned int port_index)
-#elif LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#elif VER_NON_RHEL_LT(5, 18) || RHEL_RELEASE_LT(9, 163, 0, 0)
 nfp_devlink_port_unsplit(struct devlink *devlink, unsigned int port_index,
 			 struct netlink_ext_ack *extack)
 #else
@@ -147,7 +147,7 @@ nfp_devlink_port_unsplit(struct devlink *devlink, struct devlink_port *port,
 #endif
 
 	rtnl_lock();
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#if VER_NON_RHEL_LT(5, 18) || RHEL_RELEASE_LT(9, 163, 0, 0)
 	ret = nfp_devlink_fill_eth_port_from_id(pf, port_index, &eth_port);
 #else
 	ret = nfp_devlink_fill_eth_port_from_id(pf, port, &eth_port);
