Netronome/nfp-drv-kmods

commit 3d1254c0f13e4a2a96c29a69dc1bb3e763dfed05

dim: pass dim_sample to net_dim() by reference
net_dim() is currently passed a struct dim_sample argument by value.
struct dim_sample is 24 bytes. Since this is greater 16 bytes, x86-64
passes it on the stack. All callers have already initialized dim_sample
on the stack, so passing it by value requires pushing a duplicated copy
to the stack. Either witing to the stack and immediately reading it, or
perhaps dereferencing addresses relative to the stack pointer in a chain
of push instructions, seems to perform quite poorly.

In a heavy TCP workload, mlx5e_handle_rx_dim() consumes 3% of CPU time,
94% of which is attributed to the first push instruction to copy
dim_sample on the stack for the call to net_dim():
// Call ktime_get()
  0.26 |4ead2:   call   4ead7 <mlx5e_handle_rx_dim+0x47>
// Pass the address of struct dim in %rdi
       |4ead7:   lea    0x3d0(%rbx),%rdi
// Set dim_sample.pkt_ctr
       |4eade:   mov    %r13d,0x8(%rsp)
// Set dim_sample.byte_ctr
       |4eae3:   mov    %r12d,0xc(%rsp)
// Set dim_sample.event_ctr
  0.15 |4eae8:   mov    %bp,0x10(%rsp)
// Duplicate dim_sample on the stack
 94.16 |4eaed:   push   0x10(%rsp)
  2.79 |4eaf1:   push   0x10(%rsp)
  0.07 |4eaf5:   push   %rax
// Call net_dim()
  0.21 |4eaf6:   call   4eafb <mlx5e_handle_rx_dim+0x6b>

To allow the caller to reuse the struct dim_sample already on the stack,
pass the struct dim_sample by reference to net_dim().

Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>
Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>
Reviewed-by: Shannon Nelson <shannon.nelson@amd.com>
Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
Reviewed-by: Arthur Kiyanovski <akiyano@amazon.com>
Reviewed-by: Louis Peens <louis.peens@corigine.com>
Link: https://patch.msgid.link/20241031002326.3426181-2-csander@purestorage.com
Signed-off-by: Jakub Kicinski <kuba@kernel.org>
[Louis: backport and add compat code]
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_net_compat.c
@@ -263,3 +263,17 @@ __printf(2, 3) void ethtool_sprintf(u8 **data, const char *fmt, ...)
 }
 #endif
 #endif
+
+/* kernel commit: 61bf0009a765 ("dim: pass dim_sample to net_dim() by reference")
+ * converted the 'net_dim' function to take in end_sample by reference. Add the
+ * applicable compat function code. Use this compat__ function instead of doing
+ * this everywhere in the rest of the source code.
+ */
+void compat__net_dim(struct dim *dim, const struct dim_sample *end_sample)
+{
+#ifdef COMPAT_DIM_PASS_BY_REF
+	net_dim(dim, end_sample);
+#else
+	net_dim(dim, *end_sample);
+#endif
+}

src/nfp_net_compat.h
@@ -1731,4 +1731,19 @@ static inline bool flow_rule_match_has_control_flags(struct flow_rule *rule,
 	return flow_rule_has_control_flags(match.mask->flags, extack);
 }
 #endif
+
+/**
+ *      net_dim - main DIM algorithm entry point
+ *      @dim: DIM instance information
+ *      @end_sample: Current data measurement
+ *
+ * Called by the consumer.
+ * This is the main logic of the algorithm, where data is processed in order
+ * to decide on next required action.
+ *
+ * A compat version is needed, as end_sample changed from being passed by value
+ * to being passed as reference.
+ */
+void compat__net_dim(struct dim *dim, const struct dim_sample *end_sample);
+
 #endif /* _NFP_NET_COMPAT_H_ */

src/Kbuild
@@ -215,6 +215,9 @@ endif
 # Check for DIM
 $(eval $(call add_compat_flag,$(srctree)/include/linux/dim.h,net_dim_get_rx,COMPAT_HAVE_DIM))
 
+# Check for DIM sample by reference
+$(eval $(call add_compat_flag,$(srctree)/include/linux/dim.h,"^void net_dim.*struct dim_sample \*end_sample",COMPAT_DIM_PASS_BY_REF))
+
 # Check for netif_set_real_num_queues
 $(eval $(call add_compat_flag,$(srctree)/include/linux/netdevice.h,netif_set_real_num_queues,COMPAT__HAVE_NETIF_SET_REAL_NUM_QUEUES))
 

src/nfd3/dp.c
@@ -1239,7 +1239,7 @@ int nfp_nfd3_poll(struct napi_struct *napi, int budget)
 		} while (u64_stats_fetch_retry(&r_vec->rx_sync, start));
 
 		dim_update_sample(r_vec->event_ctr, pkts, bytes, &dim_sample);
-		net_dim(&r_vec->rx_dim, dim_sample);
+		compat__net_dim(&r_vec->rx_dim, &dim_sample);
 	}
 
 	if (r_vec->nfp_net->tx_coalesce_adapt_on && r_vec->tx_ring) {
@@ -1254,7 +1254,7 @@ int nfp_nfd3_poll(struct napi_struct *napi, int budget)
 		} while (u64_stats_fetch_retry(&r_vec->tx_sync, start));
 
 		dim_update_sample(r_vec->event_ctr, pkts, bytes, &dim_sample);
-		net_dim(&r_vec->tx_dim, dim_sample);
+		compat__net_dim(&r_vec->tx_dim, &dim_sample);
 	}
 #endif
 

src/nfdk/dp.c
@@ -1348,7 +1348,7 @@ int nfp_nfdk_poll(struct napi_struct *napi, int budget)
 		} while (u64_stats_fetch_retry(&r_vec->rx_sync, start));
 
 		dim_update_sample(r_vec->event_ctr, pkts, bytes, &dim_sample);
-		net_dim(&r_vec->rx_dim, dim_sample);
+		compat__net_dim(&r_vec->rx_dim, &dim_sample);
 	}
 
 	if (r_vec->nfp_net->tx_coalesce_adapt_on && r_vec->tx_ring) {
@@ -1363,7 +1363,7 @@ int nfp_nfdk_poll(struct napi_struct *napi, int budget)
 		} while (u64_stats_fetch_retry(&r_vec->tx_sync, start));
 
 		dim_update_sample(r_vec->event_ctr, pkts, bytes, &dim_sample);
-		net_dim(&r_vec->tx_dim, dim_sample);
+		compat__net_dim(&r_vec->tx_dim, &dim_sample);
 	}
 #endif
 

commit 2470143e81de64ef9b51b52c0508ef35c4d8eebe

compat: fix `struct dim_sample` implicit declaration
Some kernels do not have the dynamic interrupt moderation library.
Protect the compat definition of `net_dim` behind the `COMPAT_HAVE_DIM`
define to avoid the implicit declaration of `struct dim_sample`.

Previously, the compiler would raise warnings on certain kernels, even
though the function was never used in those cases.

Fixes: 3d1254c0f13e ("dim: pass dim_sample to net_dim() by reference")
Signed-off-by: Ryno Swart <ryno.swart@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_net_compat.c
@@ -264,6 +264,7 @@ __printf(2, 3) void ethtool_sprintf(u8 **data, const char *fmt, ...)
 #endif
 #endif
 
+#ifdef COMPAT_HAVE_DIM
 /* kernel commit: 61bf0009a765 ("dim: pass dim_sample to net_dim() by reference")
  * converted the 'net_dim' function to take in end_sample by reference. Add the
  * applicable compat function code. Use this compat__ function instead of doing
@@ -277,3 +278,4 @@ void compat__net_dim(struct dim *dim, const struct dim_sample *end_sample)
 	net_dim(dim, *end_sample);
 #endif
 }
+#endif

src/nfp_net_compat.h
@@ -1732,6 +1732,7 @@ static inline bool flow_rule_match_has_control_flags(struct flow_rule *rule,
 }
 #endif
 
+#ifdef COMPAT_HAVE_DIM
 /**
  *      net_dim - main DIM algorithm entry point
  *      @dim: DIM instance information
@@ -1745,5 +1746,6 @@ static inline bool flow_rule_match_has_control_flags(struct flow_rule *rule,
  * to being passed as reference.
  */
 void compat__net_dim(struct dim *dim, const struct dim_sample *end_sample);
+#endif
 
 #endif /* _NFP_NET_COMPAT_H_ */